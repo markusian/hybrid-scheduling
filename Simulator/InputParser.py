ec0ba5a00c22c7eeda245fb4ccee3a2bc365e244
Harr1l <harri.laine@gmail.com>
2013-11-01 10:54:01 +0100

    InputParser returns list of task info

--- Simulator/InputParser.py
+++ Simulator/InputParser.py
@@ -1,25 +1,33 @@
+#We are using csv files for easy editing by hand
 import csv
 
-
 class InputParser(object):
+    #Parse csv file
 
+    #Give input data filename as parameter
     def __init__(self, filename):
         self.filename = filename
         self.taskList = []
 
-    def getTaskList(self):
-        return self.taskList
+    #Returns the task list with first row skipped
+    #def getTaskList(self):
+    #    return self.taskList[1:]
 
+    #Read the data from file and return it as a list
+    #Format is: tasktype,wcet,period,firstArrivalTime,interarrivalTime
+    #First row is not passed
     def getTasksFromFile(self):
-        i = True
         with open(self.filename, 'rb') as fh:
             reader = csv.reader(fh)
             for row in reader:
                 self.taskList.append(row)
+                
+        return self.taskList[1:]
 
 
+#test and print the info
 #ip = InputParser("test.csv")
 #ip.getTasksFromFile()
 
-#for i in range(1, len(ip.getTaskList())):
+#for i in range(0, len(ip.getTaskList())):
 #    print ip.getTaskList()[i]
