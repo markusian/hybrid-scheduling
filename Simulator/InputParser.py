5e7351513322e01cb0b4f93c9188dcae5b94f57b
Harr1l <harri.laine@gmail.com>
2013-11-01 11:51:23 +0100

    Some improvements to InputParser

--- Simulator/InputParser.py
+++ Simulator/InputParser.py
@@ -1,4 +1,6 @@
 #We are using csv files for easy editing by hand
+from HardTask import HardTask
+#from SoftTask import SoftTask
 import csv
 
 class InputParser(object):
@@ -7,11 +9,27 @@ class InputParser(object):
     #Give input data filename as parameter
     def __init__(self, filename):
         self.filename = filename
+        self.taskInfo = []
         self.taskList = []
 
+    #Stub for creating actual tasks from info
+    def createTask(self, row):
+        if row[0] == "hard":
+            task = HardTask(1, row[3], row[1], float(row[2]))
+        elif row[0] == "soft":
+            task = SoftTask(2, row[3], row[1], 0, row[4])
+        else:
+            return False
+        return task
+
+
+    
     #Returns the task list with first row skipped
-    #def getTaskList(self):
-    #    return self.taskList[1:]
+    def getTaskInfo(self):
+        return self.taskInfo[1:]
+
+    def getTaskList(self):
+        return self.taskList
 
     #Read the data from file and return it as a list
     #Format is: tasktype,wcet,period,firstArrivalTime,interarrivalTime
@@ -20,9 +38,12 @@ class InputParser(object):
         with open(self.filename, 'rb') as fh:
             reader = csv.reader(fh)
             for row in reader:
-                self.taskList.append(row)
+                task = self.createTask(row)
+                if task != False:
+                    self.taskList.append(task)
+                #self.taskInfo.append(row)
                 
-        return self.taskList[1:]
+        #return self.taskInfo[1:]
 
 
 #test and print the info
