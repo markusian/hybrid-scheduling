48459732451bfdaacf17353ea90ec8dc30a7e70b
Baptiste REYNAL DE SAINT MICHEL <s131795@student.dtu.dk>
2013-12-11 18:21:52 +0100

    Missing files

---
+++ New_Simulator/Queue.py
@@ -0,0 +1,52 @@
+import heapq as hq
+class FIFO(object):
+    def __init__(self):
+        self.list = []
+
+    def put(self, elem):
+        self.list.append(elem)
+
+    def isEmpty(self):
+        return len(self.list) == 0
+
+    def pop(self):
+        ret = self.list[0]
+        self.list.remove(self.list[0])
+        return ret
+
+    def first(self):
+        return self.list[0]
+
+class PriorityQueue(object):
+    def __init__(self):
+        self.list = []
+
+    def put(self, elem, priority):
+        """
+        Add an element to the list with the given priority.
+        """
+        hq.heappush(self.list, (-priority, elem))
+
+    def isEmpty(self):
+        """
+        Return true if the list is empty.
+        """
+        return len(self.list) == 0
+
+    def pop(self):
+        """
+        Remove and return the most priority element.
+        """
+        if (self.isEmpty()):
+            return None
+
+        return hq.heappop(self.list)[1]
+
+    def first(self):
+        """
+        Return the most priority element (without removing it).
+        """
+        if (self.isEmpty()):
+            return None
+
+        return self.list[0][1]
