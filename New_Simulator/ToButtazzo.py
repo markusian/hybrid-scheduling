eaa945480426b34fb8cf8efcc07059a01e419572
Ferdinando Papale <ferdinando.papale@gmail.com>
2013-12-12 10:12:17 +0100

    Added new case study + analysis script

---
+++ New_Simulator/ToButtazzo.py
@@ -0,0 +1,127 @@
+import sys
+import json
+from numpy import arange, linspace
+from Task import PeriodicTask, AperiodicTask
+from Server import PollingServer, DeferrableServer, BackgroundServer
+from Simulator import Simulator
+from Instance import Instance
+from copy import deepcopy
+from time import time
+
+OUTPUT_FOLDER = "results_q1/"
+filename = '../CaseStudies/ts12.json'
+
+PERIODIC_LOADS = [0.20, 0.40]
+MAX_TOTAL_LOAD = 0.60 #maximum total load considered
+MIN_AP_LOAD = 0.01 #minimum aperiodic load
+NUM_POINTS = 10 # number of points to consider for the aperiodic load range
+MAX_AP_LOAD = 0.30
+
+def simulationLoop(server, capacity, period, scaled, ex_time, int_time):
+        s = Simulator()
+
+        # Set the server
+        if server == 'polling':
+            s.server = PollingServer(capacity, period)
+        elif server == 'deferrable':
+            s.server = DeferrableServer(capacity, period)
+        else:
+            s.server = BackgroundServer()
+
+
+        # Load the taskset
+        for t in scaled:
+            s.tasks.append(t)
+
+        # Create the aperiodic task
+        ap = AperiodicTask("Soft", ex_time, int_time)
+        s.tasks.append(ap)
+
+
+        # RUUUUUUUUN !!!
+        s.init(until)
+        s.run()
+
+        # Compute the average response time
+        total = 0
+        average = 0
+        for i in s.statistics.instances:
+            if i.type == Instance.SOFT:
+                average = float(average * total +
+                          (i.finish - i.arrival)) / float(total + 1)
+                total += 1
+
+        #s.render(server + '.svg')
+        #sys.exit()
+        return average
+
+res = dict()
+start = time()
+file = open(filename, 'rb')
+data = json.load(file)
+taskset = list()
+periodics = data["periodics"]
+for t in periodics:
+    taskset.append(PeriodicTask(t["id"], t["wcet"], t["period"]))
+
+for p_load in PERIODIC_LOADS:
+    res[p_load] = dict()
+
+    # Scale the task set
+    scaled = deepcopy(list(taskset))
+    for t in scaled:
+        t.wcet = t.wcet * p_load
+
+    # Compute the execution time
+    #until = PeriodicTask.lcm(scaled)*NUM_HYPERPERIODS
+    print "HYPERPERIOD", PeriodicTask.lcm(scaled)
+    until = 100000
+    res[p_load]['pol'] = dict()
+    res[p_load]['def'] = dict()
+    res[p_load]['bac'] = dict()
+
+    # Compute variables for the server
+    util = PollingServer.util(p_load)
+    util_def = DeferrableServer.util2(p_load)
+
+    period = 18
+    capacity = util * period
+    capacity_def = util_def * period
+
+    print "\nPERIODIC LOAD: " + str(p_load),
+    print "\tUTIL POLLING: " + str(util),
+    print "\tUTIL DEFERRABLE: " + str(util_def)
+    print "*"*40
+
+
+    if util_def <= 0 or util_def > 1.0:
+        print "Error!!!! ", capacity_def
+        raise Exception("Problem with deferrable server")
+
+
+
+    APERIODIC_LOAD = linspace(MIN_AP_LOAD, MAX_AP_LOAD, NUM_POINTS)
+
+    for ap_load in APERIODIC_LOAD:
+        int_time = 18
+        ex_time = int_time * ap_load
+
+        print "\nAPERIODIC LOAD: " + str(ap_load),
+        print "\t\tEXECUTION TIME: " + str(ex_time)
+        avp = simulationLoop('polling', capacity, period, scaled, ex_time, int_time)
+        res[p_load]['pol'][ap_load] = avp
+
+        avd = simulationLoop('deferrable', capacity_def, period, scaled, ex_time, int_time)
+        res[p_load]['def'][ap_load] = avd
+
+        avb = simulationLoop('background', capacity, period, scaled, ex_time, int_time)
+        res[p_load]['bac'][ap_load] = avb
+
+        print "POL: " +str(avp) + "\tDEF: " +str(avd) + "\tBAC: " +str(avb)
+
+
+
+
+print "ELAPSED TIME: ",  time() - start
+fi = open(OUTPUT_FOLDER + filename.split('/')[-1].split('\\')[-1],'w')
+json.dump(res,fi)
---
+++ New_Simulator/ToGraph2.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 17:49:31 2013
+
+@author: ferdinando
+"""
+import json
+from pprint import pprint
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+OUTPUT_FOLDER = "results_q1/"
+IMAGES_FOLDER = OUTPUT_FOLDER + "images/"
+
+PERIODIC_LOADS = [0.20, 0.40]
+
+
+def getxy(li,pload):
+    ordered = OrderedDict(sorted(li.items()))
+
+    x = [float(i) for i in ordered.keys()]
+    y = [float(i) for i in ordered.values()]
+    return x,y
+
+indexes = [11,12]
+
+for i in indexes:
+    fi = open(OUTPUT_FOLDER + "ts" + str(i) + ".json")
+    di = json.load(fi)
+
+    for pload in PERIODIC_LOADS:
+
+        fa = di[str(pload)]
+        li_def, li_pol, li_bac = fa['def'], fa['pol'], fa['bac']
+
+        x_def, y_def = getxy(li_def,pload)
+        x_pol, y_pol = getxy(li_pol,pload)
+        x_bac, y_bac = getxy(li_bac,pload)
+        plt.figure()
+        plt.ylabel("Average Aperiodic Response Time")
+        plt.xlabel("Average Aperiodic Load")
+        #plt.yscale('log')
+        plt.xlim(min(x_def),max(x_def))
+        plt.plot(x_def,y_def,'-s',label="Deferrable")
+        plt.plot(x_pol,y_pol,'-o',label="Polling")
+        plt.plot(x_bac,y_bac,'-+',label="Background")
+        plt.legend()
+        name = "ts" + str(i) + "_" + str(pload)
+        plt.savefig(IMAGES_FOLDER + name  + ".pdf")
+        plt.savefig(IMAGES_FOLDER + name  + ".png")
+
